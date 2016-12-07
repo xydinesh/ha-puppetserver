099fed212639406296202a14a4b49dcd3ea10b3a
Dinesh Weerapurage <xydinesh@gmail.com>
2016-12-07 18:26:57 +0000

    adding installation files for ubuntu12.04

---
+++ ubuntu-12.04/install-puppetserver.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt-get purge --auto-remove puppet
+sudo rm -f /usr/local/bin/puppet
+wget https://apt.puppetlabs.com/puppetlabs-release-pc1-precise.deb
+sudo dpkg -i puppetlabs-release-pc1-precise.deb
+sudo apt-get update
+sudo apt-get install puppetserver
+sudo ln -s /opt/puppetlabs/bin/puppet /usr/bin/puppet
+sudo ln -s /opt/puppetlabs/bin/puppetserver /usr/bin/puppetserver
+
